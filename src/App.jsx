ce2fa9ccb04fea92d5efaec7657d33cc3de22db0
Javier Benítez <javierbentezgarca2@icloud.com>
2024-05-03 16:31:47 +0200

    Update CSS styles and add new dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import pokemon from 'pokemontcgsdk';
+import PokemonCard from './components/Card.jsx';
+import './App.css'
 
 pokemon.configure({ apiKey: '42071ed7-1765-42d0-80f2-c010f7c98927' })
 
@@ -7,22 +9,32 @@ const App = () => {
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    pokemon.card.where({ pageSize: 20, page: 1 })
+    pokemon.card.where({ pageSize: 12, page: 1 })
       .then(result => {
-        result.data.map(card => {
-          setCards(cards => [...cards, card]);})
+        const uniqueCards = {};
+        result.data.forEach(card => {
+          uniqueCards[card.id] = card;
+        });
+        setCards(Object.values(uniqueCards));
       })
   }, []);
 
   return (
     <div>
       <h1>Pokemon TCG Sets</h1>
-      {cards.map((card, index) => (
-        <div key={index}>
-          <h2>{card.name}</h2>
-          <img src={card.images.small} alt={card.name} />
-        </div>
-      ))}
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(220px, 220px))',
+        gap: '1rem',
+        justifyContent: 'center',
+        width: '100%',
+      }}>
+        {cards.map((card) => (
+          <div key={card.id}>
+            <PokemonCard card={card} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
