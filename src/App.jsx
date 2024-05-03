5a41c8bd7daf5cbe2dab0647ae1ae7781e75f334
Javier Benítez <javierbentezgarca2@icloud.com>
2024-05-03 20:50:13 +0200

    Add lazy loading for images and infinite scrolling to Pokemon card list

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import pokemon from 'pokemontcgsdk';
 import PokemonCard from './components/Card.jsx';
+import CircularProgress from '@mui/material/CircularProgress';
 import './App.css'
 
 pokemon.configure({ apiKey: '42071ed7-1765-42d0-80f2-c010f7c98927' })
 
 const App = () => {
   const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(1);
+  const loader = useRef(null);
+
+  const loadMore = useCallback(() => {
+    setPage((prevPageNumber) => prevPageNumber + 1);
+  }, []);
 
   useEffect(() => {
-    pokemon.card.where({ pageSize: 12, page: 1 })
+    const currentLoader = loader.current;
+    const observer = new IntersectionObserver(handleObserver, { root: null, rootMargin: "20px", threshold: 1.0 });
+    if (currentLoader) {
+      observer.observe(currentLoader)
+    }
+
+    return () => {
+      if (currentLoader) {
+        observer.unobserve(currentLoader);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const uniqueCards = {};
+  cards.forEach(card => {
+    uniqueCards[card.id] = card;
+  });
+
+  useEffect(() => {
+    pokemon.card.where({ pageSize: 30, page: page })
       .then(result => {
         const uniqueCards = {};
         result.data.forEach(card => {
           uniqueCards[card.id] = card;
         });
-        setCards(Object.values(uniqueCards));
+        setCards((prevCards) => [...prevCards, ...Object.values(uniqueCards)]);
       })
-  }, []);
+  }, [page]);
+
+  const handleObserver = (entities) => {
+    const target = entities[0];
+    if (target.isIntersecting) {
+      loadMore();
+    }
+  }
 
   return (
     <div>
@@ -29,11 +63,14 @@ const App = () => {
         justifyContent: 'center',
         width: '100%',
       }}>
-        {cards.map((card) => (
+        {Object.values(uniqueCards).map((card) => (
           <div key={card.id}>
             <PokemonCard card={card} />
           </div>
         ))}
+      </div>
+      <div ref={loader} style={{ margin: '1rem' }}>
+        <CircularProgress ref={loader} color="secondary" />
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,11 +7,16 @@ import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
 import PropTypes from 'prop-types';
 
 const PokemonCard = ({ card }) => {
+
+  if (!card || !card.set || !card.images || !card.cardmarket || !card.cardmarket.prices) {
+    //return null;
+  }
+  
   return (
     <Card sx={{ width: '220px' }}>
       <div>
-        <Typography level="title-lg">{card.name}</Typography>
-        <Typography level="body-sm">{card.set.name}</Typography>
+        <Typography level="title-lg">{card.name || "Unknown"}</Typography>
+        <Typography level="body-sm">{card.set?.name || "---"}</Typography>
         <IconButton
           aria-label="bookmark this card"
           variant="plain"
@@ -23,16 +28,16 @@ const PokemonCard = ({ card }) => {
         </IconButton>
       </div>
       <img
-        src={card.images.small}
+        src={card.images?.small || "https://es.web.img3.acsta.net/c_310_420/pictures/14/07/18/10/57/567741.jpg"}
         loading="lazy"
-        alt={card.name}
+        alt={card.name || "Unknown"}
         style={{ height: '250px', width: 'auto', objectFit: 'contain' }}
       />
       <CardContent orientation="horizontal">
         <div>
           <Typography level="body-xs">Total price:</Typography>
           <Typography textAlign={'left'} fontSize="lg" fontWeight="lg">
-            {card.cardmarket.prices.trendPrice}€
+            {card.cardmarket?.prices?.trendPrice || "-"}€
           </Typography>
         </div>
         <Button
